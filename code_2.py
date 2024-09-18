e168dc773f0aec86a7b48169bf98c7f92717c45b
spiritevolution <158512066+spiritevolution@users.noreply.github.com>
2024-09-18 15:52:30 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,44 +1,16 @@
 main();
-    }
-    data.forEach(item => {
-    return data;
-function main() {
-    const data = [];
-    });
-        data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {
-
-    const data = generateRandomData();
-}
-}
-        console.log("Random Number: " + item);
-
-function generateRandomData() {
-public class RandomDataGenerator {
-            data[i] = (int) (Math.random() * 100) + 1;
-    }
-        }
-        }
-        int[] data = new int[10];
-}
-    public static void main(String[] args) {
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
-
-        for (int item : data) {    }
-
-    const data = generateRandomData();
-
-function generateRandomData() {
-function main() {
     });
     const data = [];
+
+    }
+function generateRandomData() {
+    const data = generateRandomData();
 }
-main();
-    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
     return data;
+
 }
+function main() {
         console.log("Random Number: " + item);
         data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
